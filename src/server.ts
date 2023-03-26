152b19784e21b5075bd3c069b97ba482bb1cf837
Luke Morcom <lmorcom@gettalkative.com>
2023-03-26 14:27:33 +0100

    pusher + nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,23 @@
 import express from 'express';
 import Pusher from 'pusher';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; 
 
 const app = express();
+app.use(express.json());
 dotenv.config();
 
-const pusherConfig: any = {
-	appId: process.env.PUSHER_APP_ID,
-	key: process.env.PUSHER_APP_KEY,
-	secret: process.env.PUSHER_APP_SECRET,
-	cluster: process.env.PUSHER_APP_CLUSTER,
+const pusherConfig: Pusher.Options = {
+	appId: process.env.PUSHER_APP_ID!,
+	key: process.env.PUSHER_APP_KEY!,
+	secret: process.env.PUSHER_APP_SECRET!,
+	cluster: process.env.PUSHER_APP_CLUSTER!,
 };
 
 const pusher = new Pusher(pusherConfig);
 
 app.post('/twilio-message-webhook', (req, res) => {
 	const message = req.body.message;
-	pusher.trigger( 'public-chat', 'message-added', { message });
+	pusher.trigger( 'sms-channel', 'message-received', { message });
   	res.sendStatus(200);
 });
 
@@ -25,4 +26,3 @@ const port = process.env.PORT || 5000;
 app.listen(port, function () {
 	console.log(`app listening on port ${port}!`)
 });
-
